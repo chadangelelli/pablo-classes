e097e95fc210f080ece87570d218627ff8746296
Chad Angelelli <chad.angelelli@gmail.com>
2023-08-23 18:07:38 -0600

    day02: Javascript datatypes

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # pablo-classes
-Pablo Classes
-chad
-pablo
+
+> _(All times in UTC/Mexico-City)_
+
+# Day1: 2023-08-16T19:00:00
+
+## Lesson
+
+- basic setup of Neovim, NVChad, Node.js, Homebrew, Git
+- basic introduction to Neovim
+
+## Files
+
+- src/pablo/day01/day01.js
+
+# Day2: 2023-08-23T18:00:00
+
+## Lesson
+
+- introduction to data structures in Javascript
+
+## Files
+
+- src/pablo/day02/*
+- src/pablo/day02/data_types.js
---
+++ src/pablo/day02/datatypes.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/* *************************************************************** *
+ * Lesson: Basic introduction to Javascript datatypes.
+ *
+ * Topics:
+ *     1. JS "Scalars" (numbers, strings, booleans, null)
+ *     2. JS Variables
+ *     3. JS Arrays
+ *     4. JS Objects
+ *     5. JS Functions
+ *     6. That goofy '#!' thing ("shebang")
+ * *************************************************************** */
