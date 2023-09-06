075b1db57b5487c61c2b5353591a4155a686af3e
pablo cocodrilo <cocodrilo@MacBook-Air-de-pablo.local>
2023-09-06 16:55:05 -0600

    adding one more basic JS

---
+++ src/pablo/day02/arithmeticOperators.js
@@ -0,0 +1,5 @@
+// playing with the basic arithmetic operators
+console.log('--> tengo', 29 + 5, 'años'); // adding
+console.log('--> llevo', 2023-2020, 'años viviendo en Oaxaca'); //substracting
+console.log('--> porcentaje de días que hice my homework', 6/14*100); //dividing and multiplying
+console.log('--> el sobrante de dividir 11 entre 3', 11 % 3); //remainder (sobrante)
