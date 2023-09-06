3724f604062498a7295db329e70d50bcc07cb638
pablo cocodrilo <cocodrilo@MacBook-Air-de-pablo.local>
2023-09-06 17:12:21 -0600

    JS file added exploring basic concepts

---
+++ src/pablo/day02/otherBasicJSstuff.js
@@ -0,0 +1,7 @@
+//some basic JS concepts I've studied
+
+console.log('Estoy ' + 'uniendo ' +  'palabras ' + 'usando' + ' ' + '+concatenation+'); //concatenation
+
+console.log('Contando el número de caracteres en "Pablo":', 'Pablo'.length); //lenght is a property all data types contain
+
+
