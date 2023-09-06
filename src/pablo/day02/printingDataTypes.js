72145b03f053e1219b961f5fb33f132d3641b6a1
pablo cocodrilo <cocodrilo@MacBook-Air-de-pablo.local>
2023-09-06 16:19:20 -0600

    uploading one very basic js file

---
+++ src/pablo/day02/printingDataTypes.js
@@ -0,0 +1,5 @@
+/* These are some printing tests to explore data types */
+console.log(34); //printing a number
+console.log('esa es mi edad, ya estoy viejo'); // printing a string, usar 'comillas' 
+
+
