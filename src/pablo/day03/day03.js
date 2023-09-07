cd9ed02f560d459aa1f048ef05357f2e6238e112
pablo cocodrilo <cocodrilo@MacBook-Air-de-pablo.local>
2023-09-06 20:37:08 -0600

    updated structure people & music

--- src/pablo/day03/day03.js
+++ src/pablo/day03/day03.js
@@ -6,95 +6,109 @@ var people = {
     lastName: "Angelelli",
     email: "chad@tes.com",
     phone: "123-456-789",
+    favoriteGenre: "",
+    favoriteArtists: "<idArtist>",
   },
   idUser2: {
     firstName: "Pablo",
     lastName: "Cocodrilo",
     email: "pablo@something.com",
     phone: "5555-5555",
+    favoriteGenre: "",
+    favoriteArtists: "<idArtist>",
   },
   idUser3: {
     firstName: "Caro",
     lastName: "Algo",
     email: "caro@algo.com",
     phone: "248-238-238",
+    favoriteGenre: "",
+    favoriteArtists: "<idArtist>",
   },
   idUser4: {
     firstName: "Pam",
     lastName: "Redin",
     email: "pam@redin.com",
     phone: "000-999-000",
+    favoriteGenre: "",
+    favoriteArtists: "<idArtist>",
   },
 };
 
 var artists = {
   idArtist1: {
-
+    artistName: "The Clash",
+    artistGenre: "punk rock",
   }, 
   idArtist2: {
-
+    artistName: "",
+    artistGenre: "",
   }, 
   idArtist3: {
-
+    artistName: "",
+    artistGenre: "",
   }, 
   idArtist4: {
- 
+    artistName: "",
+    artistGenre: "",
   }, 
   idArtist5: {
-
+    artistName: "",
+    artistGenre: "",
   }, 
   idArtist6: {
-
+    artistName: "",
+    artistGenre: "",
   }, 
   idArtist7: {
-
+    artistName: "",
+    artistGenre: "",
   },
   idArtist8: {
-
+    artistName: "",
+    artistGenre: 0,
   },
 };
 
-
-
 var albums = {
   idAlbum1: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
   idAlbum2: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
   idAlbum3: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
   idAlbum4: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
   idAlbum5: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
   idAlbum6: {
-
+    albumTitle: "",
+    albumYear: "",
+    albumSongs: ["",],
   }, 
 };
 
 var genres = {
-  idGenre1: {
-
-  }, 
-  idGenre2: {
-
-  }, 
-  idGenre3: {
-
-  }, 
-  idGenre4: {
-
-  }, 
-  idGenre5: {
-
-  }, 
-  idGenre6: {
-
-  }, 
+  idGenre1: "", 
+  idGenre2: "", 
+  idGenre3: "", 
+  idGenre4: "", 
+  idGenre5: "", 
+  idGenre6: "", 
 };
 
