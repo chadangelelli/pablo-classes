3e3f608c7fce0c6fac173580fa38f71ea9122cb0
pablo cocodrilo <cocodrilo@MacBook-Air-de-pablo.local>
2023-09-06 19:39:39 -0600

    added modeled users list

--- src/pablo/day03/day03.js
+++ src/pablo/day03/day03.js
@@ -1,1 +1,32 @@
 // day03.js
+
+var people = {
+  idUser1: {
+    firstName: "Chad",
+    lastName: "Angelelli",
+    email: "chad@tes.com",
+    phone: "123-456-789",
+  },
+  idUser2: {
+    firstName: "Pablo",
+    lastName: "Cocodrilo",
+    email: "pablo@something.com",
+    phone: "5555-5555",
+  },
+  idUser3: {
+    firstName: "Caro",
+    lastName: "Algo",
+    email: "caro@algo.com",
+    phone: "248-238-238",
+  },
+  idUser4: {
+    firstName: "Pam",
+    lastName: "Redin",
+    email: "pam@redin.com",
+    phone: "000-999-000",
+  },
+};
+
+var music = {
+
+}
